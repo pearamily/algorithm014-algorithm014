dab50e6cae7e3074b83b8445981042aca1be63a5
刘立功 <pearamily@outlook.com>
2020-08-17 23:21:10 +0800

    求2数交集 350. 两个数组的交集 II

---
+++ Week_02/Intersect.java
@@ -0,0 +1,38 @@
+package Week_02;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/*
+求2个数组的交集
+ */
+public class Intersect {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        HashMap<Integer, Integer> appearMap = new HashMap<>();
+        for (int num : nums1) {
+            int count = appearMap.getOrDefault(num, 0) + 1;
+            appearMap.put(num, count);
+        }
+
+        int index = 0;
+        int[] resultArr = new int[nums1.length];
+        for (int num :
+                nums2) {
+            int count = appearMap.getOrDefault(num, 0);
+            if (count > 0) {
+                resultArr[index++] = num;
+                appearMap.put(num, --count);
+            } else {
+                appearMap.remove(num);
+
+            }
+
+
+        }
+        return Arrays.copyOfRange(resultArr, 0, index);
+    }
+
+}
